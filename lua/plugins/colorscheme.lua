2a0c0b3421f06497dee2e7ff5aeef949cc68565b
braidonw <braidon@braidonwhatley.com>
2024-03-19 21:47:51 +1100

    add monokai pro tokyo theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,7 +2,7 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "tokyonight",
+      colorscheme = "monokai-pro",
     },
   },
   {
@@ -16,6 +16,49 @@ return {
         highlights = {
           ["@module.elixir"] = { link = "@type" },
         },
+      })
+    end,
+  },
+  {
+    "loctvl842/monokai-pro.nvim",
+    config = function()
+      require("monokai-pro").setup({
+        styles = {
+          comment = { italic = true },
+          keyword = { italic = false, bold = false }, -- any other keyword
+          type = { italic = false }, -- (preferred) int, long, char, etc
+          storageclass = { italic = false }, -- static, register, volatile, etc
+          structure = { italic = false }, -- struct, union, enum, etc
+          parameter = { italic = false }, -- parameter pass in function
+          annotation = { italic = false },
+          tag_attribute = { italic = false }, -- attribute of tag in reactjs
+        },
+        override = function(c)
+          return {
+            IndentBlanklineChar = { fg = c.base.dimmed4 },
+            ["@module.elixir"] = { link = "@type" },
+            Comment = { fg = c.base.dimmed1, italic = true },
+          }
+        end,
+        overridePalette = function(_)
+          return {
+            dark2 = "#101014",
+            dark1 = "#16161E",
+            background = "#1A1B26",
+            text = "#C0CAF5",
+            accent1 = "#f7768e",
+            accent2 = "#7aa2f7",
+            accent3 = "#e0af68",
+            accent4 = "#9ece6a",
+            accent5 = "#0DB9D7",
+            accent6 = "#9d7cd8",
+            dimmed1 = "#737aa2",
+            dimmed2 = "#787c99",
+            dimmed3 = "#363b54",
+            dimmed4 = "#363b54",
+            dimmed5 = "#16161e",
+          }
+        end,
       })
     end,
   },
