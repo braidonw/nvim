5ca218046221c17ce4f12bc2a026cdafa118e3d8
braidonw <braidon@braidonwhatley.com>
2024-03-20 08:12:31 +1100

    customised

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,16 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+-- -- Disable continuations
+keymap.set("n", "<Leader>o", "o<Esc>^Da", opts)
+keymap.set("n", "<Leader>O", "O<Esc>^Da", opts)
+
+-- New tab
+keymap.set("n", "te", ":tabedit<Return>")
+keymap.set("n", "td", ":tabclose<Return>")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
