74a6153281820f969d8775816e846edd7eca7561
braidonw <braidon@braidonwhatley.com>
2024-03-21 08:41:11 +1100

    gruvbox theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -2,9 +2,42 @@ return {
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "solarized-osaka",
+      colorscheme = "gruvbox",
     },
   },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+    config = function()
+      require("gruvbox").setup({
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = false,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = "", -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {
+          ["@module.elixir"] = { link = "@type" },
+        },
+        dim_inactive = false,
+        transparent_mode = true,
+      })
+    end,
+  },
+
   {
     "ribru17/bamboo.nvim",
     lazy = false,
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -38,12 +38,6 @@ return {
   {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
-    opts = {
-      options = {
-        -- globalstatus = false,
-        theme = "solarized_dark",
-      },
-    },
   },
   -- filename
   {
