ec8a67a4491e20433825723903520652c667220b
braidonw <braidon@braidonwhatley.com>
2024-03-20 14:12:46 +1100

    copilot settings

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,5 +1,30 @@
 return {
   {
+    "zbirenbaum/copilot.lua",
+    cmd = "Copilot",
+    build = ":Copilot auth",
+    opts = {
+      suggestion = {
+        enabled = true,
+        auto_trigger = true,
+        debounce = 75,
+        keymap = {
+          accept = "<C-l>",
+          accept_word = false,
+          accept_line = false,
+          next = "<M-]>",
+          prev = "<M-[>",
+          dismiss = "<C-]>",
+        },
+      },
+      panel = { enabled = false },
+      filetypes = {
+        markdown = true,
+        help = true,
+      },
+    },
+  },
+  {
     "echasnovski/mini.hipatterns",
     event = "BufReadPre",
     opts = {
